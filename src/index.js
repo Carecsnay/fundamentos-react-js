4e4498cf452f1f573388b36e510d14ca28a1ec3b
Carecsnay <carecsnay1@gmail.com>
2021-02-08 19:23:01 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render{
+ReactDOM.render(
     <h1>Seja Bem-Vindo (@)!</h1>, //trecho de código JSX
-    document.getElementById('root');
-
-}
+    document.getElementById('root')
+)

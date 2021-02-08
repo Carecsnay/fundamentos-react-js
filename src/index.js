6b4c033c7e9a1cd68cdc9637e5802ff390dfe60a
Carecsnay <carecsnay1@gmail.com>
2021-02-08 19:23:47 -0300

    Import do CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './style.css';
+
 ReactDOM.render(
     <h1>Seja Bem-Vindo (@)!</h1>, //trecho de código JSX
     document.getElementById('root')

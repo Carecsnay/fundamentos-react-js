ac270500e486b7feb9f2639d3fdde69e852810b2
Carecsnay <carecsnay1@gmail.com>
2021-02-08 19:21:11 -0300

    SPA (utilizando um único HTML) na id 'root'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render{
-    <h1>Seja Bem-Vindo (@)!</h1>
-    
+    <h1>Seja Bem-Vindo (@)!</h1>, //trecho de código JSX
+    document.getElementById('root');
+
 }

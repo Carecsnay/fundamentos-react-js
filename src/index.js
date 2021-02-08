34ae152993c7fe654e941d125c53bedbb7f3a625
Carecsnay <carecsnay1@gmail.com>
2021-02-08 20:26:21 -0300

    Pausando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './../assets/logo.png'
 
 import './style.css';
 
-ReactDOM.render (
-    <h1>Seja Bem-Vindo (a)!</h1>, //trecho de código JSX
-    document.getElementById('root')
-)
 
-export default () => <img src={logo}/>
+ReactDOM.render (
+        <h1>Seja Bem-Vindo (a)!</h1>, //trecho de código JSX
+        document.getElementById('root')
+)
